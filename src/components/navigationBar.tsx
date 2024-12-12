eeeb6188786b8ec5e4ab74be81c5ad075b321109
Uzair Ahmed <essani.addy@gmail.com>
2024-12-12 13:32:36 +0500

    lst

--- src/components/navigationBar.tsx
+++ src/components/navigationBar.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
-import Image from "next/image";
-import { TiThMenu } from "react-icons/ti";
 
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { TiThMenu } from "react-icons/ti";
 
 export function SheetDemo() {
   return (
@@ -19,45 +19,37 @@ export function SheetDemo() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>
-            <Link href="/">
-              {" "}
-              <h1 className="font-black text-2xl underline pb-6">Nike</h1>
-            </Link>
-          </SheetTitle>
+          <SheetTitle></SheetTitle>
           <SheetDescription></SheetDescription>
         </SheetHeader>
-          {/* Search */}
-          <div className="flex w-FULL h-[40px] bg-primaryUzairColor  rounded-full items-center  ">
-          <Image src="/search.png" alt="search icon" width={24} height={24} />
-          <input
-            className="w-full h-full  bg-primaryUzairColor ml-1"
-            type="text"
-            placeholder="Search"
-          />
-        </div>
-        <br />
-        <ul className="space-y-5 text-bold text-xl">
+        <ul className="leading-10  ">
+          <li className="">
+            <a href="/">New & Featured</a>
+          </li>
+
           <li className="hover:underline">
-            <Link href="/">New & Featured</Link>
+            <a href="#About">Men</a>
+          </li>
+
+          <li className="hover:underline">
+            <a href="#Contact">Women</a>
           </li>
           <li className="hover:underline">
-            <Link href="/Detail">Men</Link>
+            <a href="#Contact">Kids</a>
           </li>
           <li className="hover:underline">
-            <Link href="/Detail2">Women</Link>
+            <a href="#Contact">Sale</a>
           </li>
           <li className="hover:underline">
-            <Link href="/Detail3">Kids</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/AllProducts">Sale</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/">SNKRS</Link>
+            <a href="#Contact">SNKRS</a>
           </li>
         </ul>
-      
+
+        <SheetFooter>
+          <SheetClose asChild>
+            {/* <Button type="submit">Save changes</Button> */}
+          </SheetClose>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
